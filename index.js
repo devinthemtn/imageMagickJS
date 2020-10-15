73ab61b7ba11133c9b7e40a7335daeb4f2008eb3
Thomas Koefod <tom@nativeteachingaids.com>
2020-10-15 13:02:48 -0600

    cutPoster Good

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 const fs = require("fs");
 const gm = require("gm").subClass({ imageMagick: true });
 
-const [, , doFunc, inputFile, newFile, ...args] = process.argv;
+let [, , inputFile, newFile, doFunc, ...args] = process.argv;
 
 function setFuncByName(filename) {
   const regExIsPoster = /Poster/;
   const regExIsLandscape = /_L\.\w+$/;
   const isOddPage = getPageNum(filename) % 2 !== 0;
   console.log("is Odd: ", isOddPage);
+  let doFunc = "";
   if (regExIsPoster.test(filename)) {
     // fileName is Poster Image
     if (regExIsLandscape.test(filename)) {
       // fileName is Poster & landscape
+      doFunc = "cutPosterRotateCW";
+    } else {
+      // fileName is Poster & NOT Landscape
+      doFunc = "cutPoster";
     }
   } else {
     if (regExIsLandscape.test(filename)) {
-      // fileName is in Landscape & NOT poster
-      console.log("Landscape yes");
+      // fileName is Landscape & NOT poster
+    } else {
+      // fileName NOT Landscape & NOT poster
     }
   }
+  return doFunc;
 }
 
 function getPageNum(filename) {
@@ -30,10 +37,15 @@ function getPageNum(filename) {
   return justNumA[0];
 }
 
-setFuncByName(inputFile);
 let pageNum = getPageNum(inputFile);
-console.log("pageNum: ", pageNum);
 
+// set function by inputFile name conventions
+if (typeof doFunc === "undefined") {
+  doFunc = setFuncByName(inputFile);
+}
+console.log("doFunc: ", doFunc);
+// ImageMagick transformations
+if (doFunc === "") return; // no manipulation needed exit
 if (doFunc === "thumb") {
   gm(inputFile)
     .resize(100, 100)
@@ -49,5 +61,35 @@ if (doFunc === "thumb") {
       else console.log("done");
     });
 } else if (doFunc === "posterCutRotate") {
-} else if (doFunc === "posterCut") {
+  if (pageNum % 2 === 0) {
+    //even page number
+    //cut on right
+    gm(inputFile)
+      .crop(910, 1310, 30, 60)
+      .write(newFile, (err) => {
+        if (err) console.warn("error writing", err);
+        else console.log("done");
+      });
+  } else {
+    //odd page number
+    //cut on left
+  }
+} else if (doFunc === "cutPoster") {
+  if (pageNum % 2 === 0) {
+    //even page - cut on right
+    gm(inputFile)
+      .crop(910, 1310, 0, 60)
+      .write(newFile, (err) => {
+        if (err) console.warn("error writing", err);
+        else console.log("done good");
+      });
+  } else {
+    //odd page -- cur on left
+    gm(inputFile)
+      .crop(910, 1310, 30, 60)
+      .write(newFile, (err) => {
+        if (err) console.warn("error writing", err);
+        else console.log("done good");
+      });
+  }
 }
